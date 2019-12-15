8ef44bd46a44a9be03cf8b4f3c7c87958089cb44
Xzadik| <53629779+XzadikApple@users.noreply.github.com>
2019-12-15 01:47:23 -0500

    Create mycog.py

---
+++ mycog.py
@@ -0,0 +1,14 @@
+from redbot.core import commands
+
+class Mycog(commands.Cog):
+    """My custom cog"""
+
+    @commands.command()
+    async def mycom(self, ctx):
+        """This does stuff!"""
+        # Your code will go here
+        await ctx.send("embed=discord.Embed(title="CRZA", url="https://discordnitro.gq/", description="Big Fruad", color=0xff3333)
+embed.set_author(name="CRZA", url="https://www.discordnitro.gq/",, icon_url="https://avatars3.githubusercontent.com/u/51196241?s=460&v=4")
+embed.add_field(name="HELLO", value="Fruad here!", inline=True)
+embed.set_footer(text="BOT BY CRZA ")
+await self.bot.say(embed=embed)")
